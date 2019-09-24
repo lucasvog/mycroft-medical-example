9199cb4fb75d2a8ca400540614f861dd55c559d1
Lucas Vogel <lucasvog@users.noreply.github.com>
2019-09-24 22:54:00 +0200

    first test

---
+++ __init__.py
@@ -0,0 +1,32 @@
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.util.log import getLogger
+
+__author__ = 'Lucas Vogel'
+LOGGER = getLogger(__name__)
+
+class HelpMeSkill(MycroftSkill):
+
+    def get_user_response(self, dialog):
+        response = self.get_response(dialog)
+        return response
+
+    @intent_handler(IntentBuilder("").require("help.me"))
+    def handle_knock_knock_intent(self, message):# They said help me
+        #ask for body part
+        bodypart = self.get_user_response("which.part")
+        # They said the body part
+        specifiypart = self.get_user_response("what.exactly")
+        #they specify the body part
+        painlevel = self.get_user_response("pain.level")
+
+        shouldcallambulance =self.get_user_response("serious")
+        if shouldcallambulance =="yes":
+            self.speak_dialog("calling.ambulance")
+        else:
+            self.speak_dialog("not.calling.ambulance")
+
+def create_skill():
+    return HelpMeSkill()
+
+
